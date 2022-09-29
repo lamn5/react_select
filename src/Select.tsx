b2e1e4d8799c10db63ae24defd4235eb7eec0cf4
lamn5 <noddylam24@gmail.com>
2022-09-28 20:01:04 -0400

    inital commit

---
+++ src/Select.tsx
@@ -0,0 +1,13 @@
+ type SelectOption = {
+    label: string
+    value: string
+ }
+ type SelectProps = {
+    options: SelectOption []
+    value?: SelectOption
+    onChange: (value: SelectOption | undefined) => void
+ }
+
+export function Select ( { value, onChange, options }: SelectProps) {
+
+}
